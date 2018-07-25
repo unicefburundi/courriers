ebc571ac8c5b4ce14b5417d28c91c38832732254
mbanje <mbanjejvernack@gmail.com>
2018-07-25 15:32:04 +0200

    Create first app

--- courriels/settings.py
+++ courriels/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courriels_app',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from localsettings import *
+except ImportError:
+    pass
+
+import djcelery
+djcelery.setup_loader()

743d8ace17be9ace742a25afbf7617df2e558f02
mbanje <mbanjejvernack@gmail.com>
2018-07-25 15:10:02 +0200

    First commit - Create project

---
+++ courriels/localsettings.py
@@ -0,0 +1,18 @@
+DATABASES = {
+    'default': {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'courriels_db',
+        'USER': 'mbanje',
+        'PASSWORD': 'nele2691988',
+        #'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+SITE_ID = 1
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*mw#v(ht6py)*s09ob1ttb((+sm)&-cfq)+9hyc-5hwo6ve%2q'
+
+#Token for wash
+TOKEN = 'ddbb0679611e8e2168c84279082a71c4d14ba305'

86d882eb8e0ab114649bc0dff3278d37d43a4b2f
mbanje <mbanjejvernack@gmail.com>
2018-07-26 15:25:25 +0200

    Edit admin.py file

---
+++ courriers_app/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from courriers_app.models import *
+
+
+
+admin.site.register(Sender)
+admin.site.register(StaffPosition)
+admin.site.register(Section)
+admin.site.register(Staff)
+admin.site.register(Mail)
+admin.site.register(track)
---
+++ courriers_app/models.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Sender(models.Model):
+    '''In this model, we will store providers'''
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100,blank=True)
+    phone_number = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return "{0} - Tel : {1}".format(self.first_name, self.phone_number)
+
+
+class StaffPosition(models.Model):
+    '''In this model, we will store staff functions'''
+    designation = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return "{0}".format(self.designation)
+
+class Section(models.Model):
+    '''In this model, we will store sections'''
+    designation = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return "{0}".format(self.designation)
+
+class Staff(models.Model):
+    '''In this model, we will store sections'''
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    section = models.ForeignKey(Section)
+    function = models.ForeignKey(StaffPosition)
+    email = models.EmailField(max_length=100)
+    office_phone_number = models.CharField(max_length=20)
+    mobile_phone_number = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
+
+class Mail(models.Model):
+    '''In this model, we will store mails and invoices'''
+    sender = models.ForeignKey(Sender)
+    mail = models.ImageField()
+
+    def __unicode__(self):
+        return "{0}".format(self.sender.first_name)
+
+class track(models.Model):
+    '''In this model, we will record wherever a mail went through'''
+    mail = models.ForeignKey(Mail)
+    staff = models.ForeignKey(Staff)
+    start_date = models.DateTimeField(auto_now_add=True, blank=True)
+    purpose = models.CharField(max_length=100)
+    end_date = models.DateTimeField(blank=True)
+
+    def __unicode__(self):
+        return "{0} - {1} - Start {2} - End {3}".format(self.mail.sender.first_name, self.staff.first_name, self.start_date, self.end_date)

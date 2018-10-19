0488aeca8cd7dc08cfdcade3cbb3df9fc91e459e
mbanje <mbanjejvernack@gmail.com>
2018-10-19 11:30:10 +0200

    Change from courriels to courriers

--- courriers/wsgi.py
+++ courriers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for courriels project.
+WSGI config for courriers project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "courriels.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "courriers.settings")
 
 application = get_wsgi_application()
--- courriers_app/apps.py
+++ courriers_app/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class CourrielsAppConfig(AppConfig):
-    name = 'courriels_app'
+class CourriersAppConfig(AppConfig):
+    name = 'courriers_app'

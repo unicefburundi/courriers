3607e9357134d249404a916edeffdacca896882d
mbanje <mbanjejvernack@gmail.com>
2019-12-30 11:52:56 +0200

    Change password done

--- courriers/settings.py
+++ courriers/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courriers_app',
+    'django_bootstrap_base_template',
 ]
 
 MIDDLEWARE = [
--- courriers/urls.py
+++ courriers/urls.py
@@ -26,6 +26,12 @@ urlpatterns = [
     url(r'^', include('courriers_app.urls')),
     url(r'^login/$', auth_views.login,  name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^password_change/$', auth_views.PasswordChangeView.as_view(),  name='password_change'),
+    url(r'^password_change_done/$', auth_views.PasswordChangeDoneView.as_view(),  name='password_change_done'),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(),  name='password_reset'),
+    url(r'^password_reset_done/$', auth_views.PasswordResetDoneView.as_view(),  name='password_reset_done'),
+    url(r'^password_reset_confirm/$', auth_views.PasswordResetConfirmView.as_view(),  name='password_reset_confirm'),
+    url(r'^password_reset_complete/$', auth_views.PasswordResetCompleteView.as_view(),  name='password_reset_complete'),
 ]
 
 if settings.DEBUG:

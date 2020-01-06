47bca4c877a448079ae6a047d17464b8b0ec12ed
mbanje <mbanjejvernack@gmail.com>
2020-01-06 11:07:02 +0200

    Password reset feature implemented

--- courriers/urls.py
+++ courriers/urls.py
@@ -28,10 +28,11 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^password_change/$', auth_views.PasswordChangeView.as_view(),  name='password_change'),
     url(r'^password_change_done/$', auth_views.PasswordChangeDoneView.as_view(),  name='password_change_done'),
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(),  name='password_reset'),
-    url(r'^password_reset_done/$', auth_views.PasswordResetDoneView.as_view(),  name='password_reset_done'),
-    url(r'^password_reset_confirm/$', auth_views.PasswordResetConfirmView.as_view(),  name='password_reset_confirm'),
-    url(r'^password_reset_complete/$', auth_views.PasswordResetCompleteView.as_view(),  name='password_reset_complete'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset_done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
